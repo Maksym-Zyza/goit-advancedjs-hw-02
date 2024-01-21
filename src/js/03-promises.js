130b9dda1f95da7219ce685ebe758a5791cc9c4b
Maksym-Zyza <mmziza88@gmail.com>
2024-01-21 14:19:13 +0200

    added task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,60 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+const createPromise = (position, delay) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      Math.random() > 0.3
+        ? resolve({ position, delay })
+        : reject({ position, delay });
+    }, delay);
+  });
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.target.elements;
+  const data = {
+    delay: Number(delay.value),
+    step: Number(step.value),
+    amount: Number(amount.value),
+  };
+  console.log(data);
+  if (data.delay >= 0 && data.step >= 0 && data.amount > 0) {
+    delayCreatePromises(data.delay, data.step, data.amount);
+    form.reset();
+  } else showIziToast('❗️ All values must be positive', '#fa903e');
+}
+
+function delayCreatePromises(timeout, step, amount) {
+  setTimeout(() => {
+    for (let i = 0; i < amount; i++) {
+      const promiseDelay = i === 0 ? 0 : step * i;
+      createPromise(i, promiseDelay)
+        .then(({ position, delay }) => {
+          const message = `✅ Fulfilled promise ${position} in ${delay}ms`;
+          showIziToast(message, '#9ae39c');
+        })
+        .catch(({ position, delay }) => {
+          const message = `❌ Rejected promise ${position} in ${delay}ms`;
+          showIziToast(message, '#e39c9a');
+        });
+    }
+  }, timeout);
+}
+
+function showIziToast(message, color) {
+  iziToast.show({
+    message,
+    drag: true,
+    close: false,
+    timeout: 7000,
+    position: 'topRight',
+    messageColor: 'white',
+    closeOnClick: true,
+    animateInside: true,
+    backgroundColor: color,
+  });
 }
